bb1513ebbf14dcf027b9964f6a497690943d0093
Amir Khalife <eng.amir.bu@gmail.com>
2024-06-29 19:01:21 +0330

    fix aiohttp RuntimeError: Event loop is closed

--- televi1/telegram_bot/webhook.py
+++ televi1/telegram_bot/webhook.py
@@ -12,11 +12,11 @@ from televi1.utils.decorators import require_http_methods
 
 
 def get_webhook_view(dp: Dispatcher):
-    session = AiohttpSession(proxy=settings.TELEGRAM_PROXY)
-    bot = Bot(settings.TELEGRAM_BOT_TOKEN, parse_mode=ParseMode.HTML, session=session)
-
     @require_http_methods(["POST"])
     async def webhook_view(request):
+        session = AiohttpSession(proxy=settings.TELEGRAM_PROXY)
+        bot = Bot(settings.TELEGRAM_BOT_TOKEN, parse_mode=ParseMode.HTML, session=session)
+
         if request.headers.get("x-telegram-bot-api-secret-token") != settings.TELEGRAM_WEBHOOK_SECRET:
             return HttpResponseForbidden()
         update = Update.model_validate(json.loads(request.body), context={"bot": bot})
