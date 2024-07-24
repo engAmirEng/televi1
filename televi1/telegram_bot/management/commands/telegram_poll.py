ff4cca33fd7495e09755970409c8af3824db7a09
Amir Khalife <eng.amir.bu@gmail.com>
2024-07-24 12:17:28 +0330

    fix polling for multiple bots

--- televi1/telegram_bot/management/commands/telegram_poll.py
+++ televi1/telegram_bot/management/commands/telegram_poll.py
@@ -1,5 +1,8 @@
 import asyncio
+from collections.abc import Awaitable
+from typing import Any, Callable
 
+import aiogram
 from django.core.management import BaseCommand
 
 from ... import dispatchers, models
@@ -9,16 +12,28 @@ class Command(BaseCommand):
     help = "Starts telegram bot polling"
 
     def handle(self, *args, **options):
+        HELPER_MONKEY_ATTR = "bot_obj_monkey"
         first_10_telegram_bots = list(models.TelegramBot.objects.all()[:10])
 
-        async def main() -> None:
-            polling_tasks = []
-            for i in first_10_telegram_bots:
-                bot = i.get_aiobot()
-                kw = {"aiobot": bot, "bot_obj": i}
-                polling_task = dispatchers.dp.start_polling(bot, **kw)
-                polling_tasks.append(polling_task)
+        class PollingNormalizerMiddleware(aiogram.BaseMiddleware):
+            async def __call__(
+                self,
+                handler: Callable[[aiogram.types.TelegramObject, dict[str, Any]], Awaitable[Any]],
+                event: aiogram.types.TelegramObject,
+                data: dict[str, Any],
+            ) -> Any:
+                bot: aiogram.Bot = data["bot"]
+                bot_obj = getattr(bot, HELPER_MONKEY_ATTR)
+                data.update({"aiobot": bot, "bot_obj": bot_obj})
+                return await handler(event, data)
 
-            await asyncio.gather(*polling_tasks)
+        async def main() -> None:
+            aiobots = []
+            for i in first_10_telegram_bots:
+                aiobot = i.get_aiobot()
+                setattr(aiobot, HELPER_MONKEY_ATTR, i)
+                aiobots.append(aiobot)
+            dispatchers.dp.update.middleware._middlewares.insert(0, PollingNormalizerMiddleware())
+            await dispatchers.dp.start_polling(*aiobots)
 
         asyncio.run(main())
